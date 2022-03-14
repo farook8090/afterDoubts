0620cfbd2cf591f3e81cbc453fa398acb5c71c32
Farook Mohammad <97977293+farook8090@users.noreply.github.com>
2022-03-14 16:07:38 +0530

    Add files via upload

---
+++ afterDoubts/Keyboard/keyboard.js
@@ -0,0 +1,11 @@
+// !This function is used to get small alphabet printed on the place on big alphabet.
+
+// ! function name is given on the element which we want to make appear when clicked on over.
+
+// & onclick
+
+function changealphabet(small_alphabet) {
+  // This function will make the small alphabet get print on big alphabet.
+  document.getElementById("big-image").src = "img/"+small_alphabet;
+  // alert('error');
+}
---
+++ afterDoubts/Travel/travel.js
@@ -0,0 +1,11 @@
+// !Price change when clicked on the radio button.
+
+// ?function name is changeprice(price)
+
+function changeprice(price) {
+  // Shown in this ID.
+  document.getElementById("result").innerHTML = "₹" + price;
+
+  // ?..Worked Smoothly
+    // alert('Worked..Bruh but price not changing.');
+}
